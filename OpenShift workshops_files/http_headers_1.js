fe6769238a44fdd79b3d2861ceeda2fd47894a32
s2977943 <w.dovey@griffith.edu.au>
2017-05-26 11:15:15 +1000

    first commit

---
+++ OpenShift workshops_files/http_headers_1.js
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AllAccessDisabled</Code><Message>All access to this object has been disabled</Message><RequestId>3F25930CD23D6794</RequestId><HostId>fK+rVDwRitEHMAvufQOqUHSboZfdKuFfkYbFkRWCLJavKnu2N5I6LR7Nn+5M4Li/QiFf5ZBdlP4=</HostId></Error>
